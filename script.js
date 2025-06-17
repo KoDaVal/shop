3d77f099491b49ff679ae97f4651726a627c19a6
KoDaVal <99213984+KoDaVal@users.noreply.github.com>
2025-06-17 13:01:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.querySelectorAll('.agregar').forEach(boton => {
-    boton.addEventListener('click', () => {
-        alert('Producto agregado al carrito!');
+    boton.addEventListener('click', (event) => {
+        const producto = event.target.parentElement.querySelector('h2').innerText;
+        let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+        carrito.push(producto);
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        alert(`${producto} agregado al carrito!`);
     });
 });
